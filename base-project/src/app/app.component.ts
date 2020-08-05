3e7db1f6c3571c032bbbd87b2f0fa061b825fbc7
PaoRivas <paoa.rivasm@gmail.com>
2020-08-05 00:14:51 -0400

    1 binaries uploaded

--- base-project/src/app/app.component.ts
+++ base-project/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implement OnInit {
   title = 'curso-angular';
+
+  ngOnInit(){
+
+    const aux = [1,2,3,4,5,6,7,8];
+
+
+  }
 }
