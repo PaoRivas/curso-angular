ee1e7d34219823ed050e7a376c2b8f21a4872221
PaoRivas <paoa.rivasm@gmail.com>
2020-08-17 16:20:19 -0400

    Input directive

--- src/app/directives/ng-color.directive.ts
+++ src/app/directives/ng-color.directive.ts
@@ -1,18 +1,21 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appNgColor]'
 })
 export class NgColorDirective {
 
+  @Input('appNgColor') color:string;
+  @Input() secondColor:string;
+
   @HostListener('mouseenter') onMouseEnter(){
     console.log('MOUSE ENTER');
-    this.changeColor('yellow');
+    this.changeColor(this.color);
   }
 
   @HostListener('mouseleave') onMouseLeave(){
     console.log('MOUSE LEAVE');
-    this.changeColor('red');
+    this.changeColor(this.secondColor);
   }
 
   constructor(public e1: ElementRef) { 
