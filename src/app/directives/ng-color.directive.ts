a00d823577245d28c4f6bbed636c29d0f4060871
PaoRivas <paoa.rivasm@gmail.com>
2020-08-17 15:49:15 -0400

    HostListener

--- src/app/directives/ng-color.directive.ts
+++ src/app/directives/ng-color.directive.ts
@@ -1,13 +1,26 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appNgColor]'
 })
 export class NgColorDirective {
 
-  constructor(e1: ElementRef) { 
-      console.log('Element Ref:', e1);
-      e1.nativeElement.style.backgroundColor = 'yellow';
+  @HostListener('mouseenter') onMouseEnter(){
+    console.log('MOUSE ENTER');
+    this.changeColor('yellow');
   }
 
+  @HostListener('mouseleave') onMouseLeave(){
+    console.log('MOUSE LEAVE');
+    this.changeColor('red');
+  }
+
+  constructor(public e1: ElementRef) { 
+      console.log('Element Ref:', e1);
+      this.changeColor('yellow');
+  }
+
+  changeColor(color:string){
+    this.e1.nativeElement.style.backgroundColor = color;
+  }
 }
