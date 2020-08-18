6d6072d3c5e6b9321dcd4e6cf131fd672e7c5846
PaoRivas <paoa.rivasm@gmail.com>
2020-08-18 16:37:06 -0400

    output directive

---
+++ src/app/directives/ng-focus.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, Output, EventEmitter, HostListener, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appNgFocus]'
+})
+export class NgFocusDirective {
+
+  @Output() appNgFocus = new EventEmitter;
+
+  @HostListener('document:click', ['$event', '$event.target'])
+  clickOutside(event, eventTarget){
+    console.log('CLCLCLCL',this.el.nativeElement.contains(eventTarget))
+
+    const aux = this.el.nativeElement.contains(eventTarget);
+
+    if(!aux){
+      this.appNgFocus.emit('se hizo click afuera');
+    }
+  }
+
+  constructor(public el:ElementRef) { }
+
+}
