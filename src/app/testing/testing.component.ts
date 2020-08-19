d8c3813c7ca279e2bea9c5b1621db87b40931707
PaoRivas <paoa.rivasm@gmail.com>
2020-08-19 15:03:53 -0400

    1 file modified

--- src/app/testing/testing.component.ts
+++ src/app/testing/testing.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnChanges, DoCheck,AfterContentInit,AfterContentChecked,AfterViewInit,AfterViewChecked,OnDestroy } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, DoCheck,AfterContentInit,AfterContentChecked,AfterViewInit,AfterViewChecked,OnDestroy, SimpleChange } from '@angular/core';
 
 @Component({
   selector: 'app-testing',
