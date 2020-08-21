3f1db1183701e3e6fdfe52965d76a6fc5c786e80
PaoRivas <paoa.rivasm@gmail.com>
2020-08-21 14:31:21 -0400

    finished

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedComponent } from './componente/shared.component';
+import { SharedDirective } from './shared.directive';
+import { SharedPipe } from './shared.pipe';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    SharedComponent
+    SharedComponent,
+    SharedDirective,
+    SharedPipe
   ],
   exports: [
-    SharedComponent
+    SharedComponent,
+    SharedDirective,
+    SharedPipe
   ]
 })
 export class SharedModule { }
