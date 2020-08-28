c7ab0dc6655c000181f051514ce1d28af74c4ba9
PaoRivas <paoa.rivasm@gmail.com>
2020-08-27 20:49:47 -0400

    few changes for second task

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -6,7 +6,7 @@ import { PagesComponent } from './pages/pages.component';
 const routes: Routes = [
   {path: '', component: PagesComponent,
   children: [
-    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
     {path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)},
     {path: 'list', loadChildren: () => import('./list/list.module').then(m => m.ListModule)}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagesComponent } from './pages/pages.component';
+import { PagesRoutingModule } from './pages-routing.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    PagesRoutingModule
   ],
   declarations: [PagesComponent]
 })
