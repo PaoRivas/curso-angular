3e70383ea945da65752a88dd188f8c711d8606bc
PaoRivas <paoa.rivasm@gmail.com>
2020-09-14 21:51:33 -0400

    getProductsById 10/09

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -35,7 +35,8 @@ export class AdminComponent implements OnInit, OnDestroy {
 
   loadProduct(): void {
     this.products = [];
-    this.productGetSubs = this.productService.getProducts().subscribe(res => {
+    const userId = localStorage.getItem('userId');
+    this.productGetSubs = this.productService.getProductsById(userId).subscribe(res => {
       Object.entries(res).map((p: any) => this.products.push({id: p[0], ...p[1]}));
     });
   }
@@ -58,7 +59,8 @@ export class AdminComponent implements OnInit, OnDestroy {
   }
 
   onUpdateProduct(): void {
-    this.productUpdateSubs = this.productService.updateProduct(this.idEdit, this.productForm.value).subscribe(
+    this.productUpdateSubs = this.productService.updateProduct(this.idEdit, {...this.productForm.value,
+    ownerId: localStorage.getItem('userId')}).subscribe(
       res => {
         console.log('RESP UPDATE: ', res);
         this.loadProduct();
@@ -72,7 +74,8 @@ export class AdminComponent implements OnInit, OnDestroy {
   onEnviar2(){
     console.log('FORM GROUP: ', this.productForm.value);
 
-    this.productSubs = this.productService.addProduct(this.productForm.value).subscribe(
+    this.productSubs = this.productService.addProduct({...this.productForm.value,
+    ownerId: localStorage.getItem('userId')}).subscribe(
       res => {
         console.log('RESP: ', res);
         this.loadProduct();
